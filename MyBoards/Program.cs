8ef15f6fd354d300caa9448ea51ba37b9ee1aad7
RogerWitkowski <witkowski.roger@gmail.com>
2022-09-20 14:26:58 +0200

    pending migration added

--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyBoardsDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsSQLConnection"));
+});
 
 var app = builder.Build();
 
@@ -13,7 +20,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//app.UseAuthorization();
 
-app.UseAuthorization();
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetService<MyBoardsDbContext>();
 
+var pendingMigrations = dbContext.Database.GetPendingMigrations();
+if (pendingMigrations.Any())
+{
+    dbContext.Database.Migrate();
+}
 app.Run();
