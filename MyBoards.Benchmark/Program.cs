dc08cd4f441293876b7ed49d2417410f91aaa7b3
RogerWitkowski <witkowski.roger@gmail.com>
2022-09-23 14:08:58 +0200

    tracking benchmark created

---
+++ MyBoards.Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using MyBoards.Benchmark;
+
+Console.WriteLine("Benchmark Created!");
+
+BenchmarkRunner.Run<TrackingBenchmark>();
---
+++ MyBoards.Benchmark/TrackingBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Data;
+
+namespace MyBoards.Benchmark
+{
+    [MemoryDiagnoser]
+    public class TrackingBenchmark
+    {
+        [Benchmark]
+        public int WithTracking()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyBoardsDbContext>()
+                .UseSqlServer("Server=localhost;Database=MyBoardEF_DB;Trusted_Connection=True;MultipleActiveResultSets=True");
+
+            var _dbContext = new MyBoardsDbContext(optionBuilder.Options);
+
+            var comments = _dbContext.Comments
+                .ToList();
+            return comments.Count;
+        }
+
+        [Benchmark]
+        public int WithNoTracking()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyBoardsDbContext>()
+                .UseSqlServer("Server=localhost;Database=MyBoardEF_DB;Trusted_Connection=True;MultipleActiveResultSets=True");
+
+            var _dbContext = new MyBoardsDbContext(optionBuilder.Options);
+
+            var comments = _dbContext.Comments
+                .AsNoTracking()
+                .ToList();
+
+            return comments.Count;
+        }
+    }
+}
